1198c153a7b8ee77dfa85ec72c4175b74f4e88cb
Pratyush Kulshreshtha <pratyushkulshreshtha35565@gmail.com>
2025-12-06 19:27:01 +0530

    move firebase keys to .env

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,19 +1,19 @@
 // src/firebase.ts
 import { initializeApp } from "firebase/app";
-import { getDatabase } from "firebase/database";
 import { getAuth } from "firebase/auth";
+import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCNlStMttfkXHfpUYtoLipM61VSYWx7xQA",
-  authDomain: "smart-irrigation-5450e.firebaseapp.com",
-  databaseURL: "https://smart-irrigation-5450e-default-rtdb.firebaseio.com",
-  projectId: "smart-irrigation-5450e",
-  storageBucket: "smart-irrigation-5450e.firebasestorage.app",
-  messagingSenderId: "631927372964",
-  appId: "1:631927372964:web:ec817d7b376daed2297a31",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  databaseURL: import.meta.env.VITE_FIREBASE_DATABASE_URL,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
 
-export const db = getDatabase(app);
 export const auth = getAuth(app);
+export const db = getDatabase(app);
